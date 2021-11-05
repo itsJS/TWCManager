dd68ac428b3b2618dd0b23d504a864217835bbbf
Private JS <privateJS@Josephs-MacBook-Pro.local>
2021-11-05 20:01:49 +0100

    reconfigure RS485

--- lib/TWCManager/Interface/RS485.py
+++ lib/TWCManager/Interface/RS485.py
@@ -63,8 +63,17 @@ class RS485:
 
     def read(self, len):
         # Read the specified amount of data from the serial interface
-        print("READ")
-        return self.ser.read(len)
+        #return self.ser.read(len)
+
+        
+        # Read our buffered messages. We simulate this by making a copy of the
+        # current message buffer, clearing the shared message buffer and then
+        # returning the copied message to TWCManager. This is what it would look
+        # like if we read from a serial interface
+        localMsgBuffer = self.msgBuffer
+        self.msgBuffer = None
+        logger.log(logging.INFO9, "Rx@: " + self.master.hex_str(localMsgBuffer))
+        return localMsgBuffer
 
     def send(self, msg):
         # Send msg on the RS485 network. We'll escape bytes with a special meaning,
